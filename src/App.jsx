efbb4d7ca83a3d7b4f511a35dfd55f023fc43e7f
ThePrince-Verma <prince.pv759@gmail.com>
2024-10-27 08:45:37 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import Header from "./components/Header";
+import Meals from "./components/Meals";
+import { CartContextProvider } from "./components/store/CartContext";
+import { UserProgressContextProvider } from './components/store/UserProgressContextProvider';
+import Cart from './components/Cart';
+import Checkout from './components/Checkout';
+
+function App() {
+  return (
+    <UserProgressContextProvider>
+    <CartContextProvider>
+    <Header />
+    <Meals />
+    <Cart />
+    <Checkout />
+    </CartContextProvider>
+    </UserProgressContextProvider>
+  );
+}
+
+export default App;
